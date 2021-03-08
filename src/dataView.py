731f561aff814c63531b961f4023025b0503e640
Etienne Roberge <e.roberge.er@gmail.com>
2021-03-08 08:21:09 -0500

    dfg

--- src/dataView.py
+++ src/dataView.py
@@ -5,6 +5,7 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 import time
+import math
 
 #from graphical.plot3d import Plot3d
 import generateFunction
@@ -16,8 +17,14 @@ from scipy import interpolate
 allData = []
 allForces = []
 allDataName = []
+allDataPosition = []
 allDataZero = []
 
+def getPositionInFilename(fileName):
+    test = fileName.split("_")
+    posX = int(test[1])
+    posY = int(test[2].split(".")[0])
+    return posX, posY
 
 def loadAllData():
     onlyFiles = [f for f in listdir("./logging") if isfile(join("./logging", f))]
@@ -44,6 +51,7 @@ def loadAllData():
 
             allForces.append(currentForce)
             allDataName.append(file)
+            allDataPosition.append(getPositionInFilename(file))
             allData.append(currentData)
 
 
@@ -87,12 +95,158 @@ def plotSomething(m):
     plt.show()
 
 
+def letsTrySoloFunction(leFunctions, force):
+    taxelValue = np.zeros((7, 4))
+    for i in range(taxelValue.shape[0]):
+        for ii in range(taxelValue.shape[1]):
+            a, b, c, d, e, f = leFunctions[(i, ii)]
+            taxelValue[i, ii] = generateFunction.fun(force, a, b, c, d, e, f)
+
+    #fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    #X = np.arange(taxelValue.shape[1])
+    #Y = np.arange(taxelValue.shape[0])
+    #X, Y = np.meshgrid(X, Y)
+
+    #xnew, ynew = np.mgrid[0:6:80j, 0:3:80j]
+    #tck = interpolate.bisplrep(Y, X, taxelValue, s=0)
+    #znew = interpolate.bisplev(xnew[:, 0], ynew[0, :], tck)
+
+    #surf = ax.plot_surface(xnew, ynew, znew, cmap=cm.coolwarm,
+    #                       linewidth=0, antialiased=True)
+    #plt.show()
+
+    return taxelValue
+
+def letsTryInterpolation(lesFunctions, force, posX, posY):
+    base = 4.0
+
+    #Get the square
+    x0 = base * math.floor(posX/base)
+    x1 = x0 + base
+    y0 = base * math.floor(posY/base)
+    y1 = y0 + base
+
+    #GetTheIndexes
+    x0y0_i = allDataPosition.index((x0, y0))
+    x1y0_i = allDataPosition.index((x1, y0))
+    x0y1_i = allDataPosition.index((x0, y1))
+    x1y1_i = allDataPosition.index((x1, y1))
+
+    #GetTheRatios
+    x0_r = (posX-x0)/(x1-x0)
+    x1_r = 1.0 - x0_r
+    y0_r = (posY-y0)/(y1-y0)
+    y1_r = 1.0 - y0_r
+
+    x0y0_r = x0_r * y0_r
+    x1y0_r = x1_r * y0_r
+    x0y1_r = x0_r * y1_r
+    x1y1_r = x1_r * y1_r
+
+    #getResultsOfEachTaxel
+    x0y0_v = letsTrySoloFunction(taxelFunctions[allDataName[x0y0_i]], force)
+    x1y0_v = letsTrySoloFunction(taxelFunctions[allDataName[x1y0_i]], force)
+    x0y1_v = letsTrySoloFunction(taxelFunctions[allDataName[x0y1_i]], force)
+    x1y1_v = letsTrySoloFunction(taxelFunctions[allDataName[x1y1_i]], force)
+
+    #ultimateResult
+    taxelValue = (x0y0_r * x0y0_v) + (x1y0_r * x1y0_v) + (x0y1_r * x0y1_v) + (x1y1_r * x1y1_v)
+
+
+    #plot
+
+    # set up a figure twice as wide as it is tall
+    fig = plt.figure()
+
+    ax = fig.add_subplot(2, 4, 1, projection='3d')
+    X = np.arange(taxelValue.shape[1])
+    Y = np.arange(taxelValue.shape[0])
+    X, Y = np.meshgrid(X, Y)
+
+    xnew, ynew = np.mgrid[0:6:80j, 0:3:80j]
+    tck = interpolate.bisplrep(Y, X, x0y0_v, s=0)
+    znew = interpolate.bisplev(xnew[:, 0], ynew[0, :], tck)
+
+    surf = ax.plot_surface(xnew, ynew, znew, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=True)
+    ax.set_zlim(-100.01, 3000.01)
+
+#----
+    ax = fig.add_subplot(2, 4, 2, projection='3d')
+    X = np.arange(taxelValue.shape[1])
+    Y = np.arange(taxelValue.shape[0])
+    X, Y = np.meshgrid(X, Y)
+
+    xnew, ynew = np.mgrid[0:6:80j, 0:3:80j]
+    tck = interpolate.bisplrep(Y, X, x0y1_v, s=0)
+    znew = interpolate.bisplev(xnew[:, 0], ynew[0, :], tck)
+
+    surf = ax.plot_surface(xnew, ynew, znew, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=True)
+    ax.set_zlim(-100.01, 3000.01)
+#----
+    ax = fig.add_subplot(2, 4, 5, projection='3d')
+    X = np.arange(taxelValue.shape[1])
+    Y = np.arange(taxelValue.shape[0])
+    X, Y = np.meshgrid(X, Y)
+
+    xnew, ynew = np.mgrid[0:6:80j, 0:3:80j]
+    tck = interpolate.bisplrep(Y, X, x1y0_v, s=0)
+    znew = interpolate.bisplev(xnew[:, 0], ynew[0, :], tck)
+
+    surf = ax.plot_surface(xnew, ynew, znew, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=True)
+    ax.set_zlim(-100.01, 3000.01)
+#----
+    ax = fig.add_subplot(2, 4, 6, projection='3d')
+    X = np.arange(taxelValue.shape[1])
+    Y = np.arange(taxelValue.shape[0])
+    X, Y = np.meshgrid(X, Y)
+
+    xnew, ynew = np.mgrid[0:6:80j, 0:3:80j]
+    tck = interpolate.bisplrep(Y, X, x1y1_v, s=0)
+    znew = interpolate.bisplev(xnew[:, 0], ynew[0, :], tck)
+
+    surf = ax.plot_surface(xnew, ynew, znew, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=True)
+    ax.set_zlim(-100.01, 3000.01)
+
+
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    X = np.arange(taxelValue.shape[1])
+    Y = np.arange(taxelValue.shape[0])
+    X, Y = np.meshgrid(X, Y)
+
+    xnew, ynew = np.mgrid[0:6:80j, 0:3:80j]
+    tck = interpolate.bisplrep(Y, X, taxelValue, s=0)
+    znew = interpolate.bisplev(xnew[:, 0], ynew[0, :], tck)
+
+    surf = ax.plot_surface(xnew, ynew, znew, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=True)
+    ax.set_zlim(-100.01, 3000.01)
+
+
+    plt.show()
+
+    pass
+
+
 if __name__ == '__main__':
     loadAllData()
     t, d = zeroAllData(allDataZero, allData)
 
-    generateFunction.generateFunction(d[0], allForces[0])
+    taxelFunctions = dict()
 
+    for i in range(len(d)):
+        functions = generateFunction.generateFunction(d[i], allForces[i])
+        taxelFunctions[allDataName[i]] = functions
+
+#    for i in range(len(d)):
+#        letsTrySoloFunction(taxelFunctions[allDataName[i]], 1.0)
+
+    letsTryInterpolation(taxelFunctions, 25.12, 6.3, 20.6)
+
+    pass
 #    for i in range(len(d)):
 #        a = Plot3d(d[i], allDataName[i])
 #        while a.hasNext:
--- src/generateFunction.py
+++ src/generateFunction.py
@@ -1,34 +1,33 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
-
 from matplotlib import rcParams
-rcParams['figure.figsize'] = (10, 6)
-rcParams['legend.fontsize'] = 16
-rcParams['axes.labelsize'] = 16
-
 from scipy.optimize import least_squares, curve_fit
 
 
 def fun(x, a, b, c, d, e, f):
-    return a*x + b*x**2 + c * x**3 + d * x**4 + e * x**5 + f
+    return a * x + b * x ** 2 + c * x ** 3 + d * x ** 4 + e * x ** 5 + f
+
 
 def generateFunction(data, force):
-    #x0 = np.ones(3)
-    #res_lsq = least_squares(fun, x0, args=(len(force), y_train))
-    #res_robust = least_squares(fun, x0, loss='soft_l1', f_scale=0.1, args=(t_train, y_train))
 
-    t = np.array(force)
-    y = data[:, 6, 2]
+    functionMap = dict()
 
-    x0 = np.ones(4)
-    res_robust, _ = curve_fit(fun, t, y)
+    for i in range(7):
+        for ii in range(4):
 
-    t_test = np.linspace(t.min(), t.max(), 300)
-    a, b, c, d, e, f = res_robust
-    result = fun(t_test, a, b, c, d, e, f)
-    #y_robust = generate_data(t_test, *res_robust.x)
+            t = np.array(force)
+            y = data[:, i, ii]
 
-    plt.plot(t, y)
-    plt.plot(t_test, result, label='robust lsq')
-    plt.show()
+            res_robust, _ = curve_fit(fun, t, y)
+
+            t_test = np.linspace(t.min(), t.max(), 300)
+            a, b, c, d, e, f = res_robust
+            result = fun(t_test, a, b, c, d, e, f)
+
+            #plt.plot(t, y)
+            #plt.plot(t_test, result)
+            #plt.show()
+
+            functionMap[(i, ii)] = [a, b, c, d, e, f]
+
+    return functionMap
